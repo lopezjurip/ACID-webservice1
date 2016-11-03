1ece474301936d1def3ff26c57522c514689622a
Patricio López <lopezjuripatricio@gmail.com>
2016-11-03 13:30:30 -0300

    Setup root redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: redirect('/rest/login')
+
   namespace :rest do
     get 'login', to: 'login#index', format: 'html'
     post 'login', to: 'login#create', format: 'json'
